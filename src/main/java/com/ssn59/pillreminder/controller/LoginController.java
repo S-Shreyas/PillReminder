2ba156c2bebde7fec873c2ba2ce845b12bb665fd
Shreyas SEETHARAMAN <sseetharaman@xhxm7wf217.ad.mdsol.com>
2022-09-08 16:57:34 +0530

    project commit

--- src/main/java/com/ssn59/pillreminder/controller/LoginController.java
+++ src/main/java/com/ssn59/pillreminder/controller/LoginController.java
@@ -2,11 +2,14 @@ package com.ssn59.pillreminder.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ssn59.pillreminder.User;
 import com.ssn59.pillreminder.services.UserServicesImpl;
 import com.ssn59.pillreminder.userlogindto.UserLoginDTO;
 
@@ -15,6 +18,8 @@ public class LoginController {
 
 	@Autowired
 	UserServicesImpl userServicesImpl;
+
+	private String userEmail;
 
 	@ModelAttribute("user")
 	public UserLoginDTO userLoginDTO() {
@@ -36,22 +41,18 @@ public class LoginController {
 	@PostMapping("/register")
 	public String userRegistration(@ModelAttribute("user") UserLoginDTO userLoginDTO) {
 
-		if(userServicesImpl.userExist(userLoginDTO.getEmail())) {
-			
+		if (userServicesImpl.userExist(userLoginDTO.getEmail())) {
+
 			return "redirect:/register?erroremail";
-		}else if(userServicesImpl.phoneExists(userLoginDTO.getPhone())){
-			
+		} else if (userServicesImpl.phoneExists(userLoginDTO.getPhone())) {
+
 			return "redirect:/register?errorphone";
-			
+
 		}
-		
-		
+
 		userServicesImpl.save(userLoginDTO);
-		
-		
+
 		return "redirect:/register?success";
-		
-		
 
 	}
 
@@ -61,19 +62,37 @@ public class LoginController {
 	}
 
 	@PostMapping("/login")
-	public String logger(@RequestParam String email, @RequestParam String pass) {
+	public String logger(Model model, @RequestParam String email, @RequestParam String pass) {
 
 		Boolean tracker = userServicesImpl.userValidation(email, pass);
 
 		if (tracker) {
+
+			userEmail = email;
+			User user = new User();
+			System.out.println(userEmail);
+			model.addAttribute("user", user);
+			model.addAttribute("users", userServicesImpl.viewCurrentPerson(userEmail));
 			return "homepage";
 		}
 		return "redirect:/login?error";
 
 	}
+
 	@GetMapping("/forgotpass")
 	public String resetPassword() {
 		return "forgotpass";
+	}
+
+	@RequestMapping("/logout")
+	public String getHomepage() {
+		
+		
+		
+		userEmail=null;
+
+		return "login";
+
 	}
 
 //	@PutMapping("/resetpass")
--- src/main/java/com/ssn59/pillreminder/services/UserServicesImpl.java
+++ src/main/java/com/ssn59/pillreminder/services/UserServicesImpl.java
@@ -1,13 +1,7 @@
 package com.ssn59.pillreminder.services;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.ssn59.pillreminder.User;
 import com.ssn59.pillreminder.userlogindao.UserLoginDAO;
@@ -48,30 +42,15 @@ public class UserServicesImpl {
 		return userLoginDAO.save(user);
 	}
 
-//
-//	public String changePass(@RequestBody Map<String, String> body) {
-//
-//		int validationID = userValidation(body.get("email"), body.get("pass"));
-//
-//		if (validationID == 1) {
-//
-//			String newPass = body.get("newpass");
-//
-//			Optional<User> val = userLoginDAO.findByEmail();
-//			User dto = val.get();
-//
-//			dto.setEmail(body.get("email"));
-//			dto.setPass(newPass);
-//
-//			userLoginDAO.save(dto);
-//
-//			return "Success";
-//
-//		}
-//
-//		return "Invalid password";
-//
-//	}
+
+	public String changePass(UserLoginDTO userLoginDTO) {
+		
+		
+		
+		
+		return "";
+
+	}
 	public Boolean userExist(String email) {
 
 		return userLoginDAO.existsByEmail(email);
@@ -97,13 +76,8 @@ public class UserServicesImpl {
 		return false;
 	}
 
-	public ModelAndView viewOne(@PathVariable String email) {
-
-		ModelAndView mav = new ModelAndView("ViewProfile");
-		Optional<User> dto = userLoginDAO.findById(email);
-		List<User> val = dto.map(Collections::singletonList).orElseGet(Collections::emptyList);
-		mav.addObject("user", val);
-		return mav;
+	public User viewCurrentPerson(String email) {
+		
+		return userLoginDAO.findByEmail(email);
 	}
-
 }
